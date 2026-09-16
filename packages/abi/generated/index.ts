05c886f97c3e13bf40e27cbd5cdc0ff539c4cee9
Neeraj Sathish Kumar <neerajcodz@gmail.com>
2026-09-16 20:11:59 +0530

    feat: publish generated contract abis

---
+++ packages/abi/generated/index.ts
@@ -0,0 +1,314 @@
+import AavePoolMock from "./AavePoolMock.json" with { type: "json" };
+import AaveV3Adapter from "./AaveV3Adapter.json" with { type: "json" };
+import AccessControl from "./AccessControl.json" with { type: "json" };
+import AggregatorMock from "./AggregatorMock.json" with { type: "json" };
+import ChainlinkOracle from "./ChainlinkOracle.json" with { type: "json" };
+import ChainlinkOracleTest from "./ChainlinkOracleTest.json" with { type: "json" };
+import CircuitBreaker from "./CircuitBreaker.json" with { type: "json" };
+import CrossChainRouter from "./CrossChainRouter.json" with { type: "json" };
+import DeltaMath from "./DeltaMath.json" with { type: "json" };
+import DeltaMathHarness from "./DeltaMathHarness.json" with { type: "json" };
+import DeltaMathTest from "./DeltaMathTest.json" with { type: "json" };
+import ECDSA from "./ECDSA.json" with { type: "json" };
+import EIP712 from "./EIP712.json" with { type: "json" };
+import ERC165 from "./ERC165.json" with { type: "json" };
+import ERC20 from "./ERC20.json" with { type: "json" };
+import EmergencyAutomation from "./EmergencyAutomation.json" with { type: "json" };
+import EmergencyControlsTest from "./EmergencyControlsTest.json" with { type: "json" };
+import EmergencyUnwind from "./EmergencyUnwind.json" with { type: "json" };
+import EmergencyUnwindTest from "./EmergencyUnwindTest.json" with { type: "json" };
+import FeeToken from "./FeeToken.json" with { type: "json" };
+import HealthFactorMath from "./HealthFactorMath.json" with { type: "json" };
+import HealthFactorMathHarness from "./HealthFactorMathHarness.json" with { type: "json" };
+import HealthFactorMathTest from "./HealthFactorMathTest.json" with { type: "json" };
+import HedgeAdapterMock from "./HedgeAdapterMock.json" with { type: "json" };
+import HedgeManager from "./HedgeManager.json" with { type: "json" };
+import HedgeManagerTest from "./HedgeManagerTest.json" with { type: "json" };
+import HedgePositionMock from "./HedgePositionMock.json" with { type: "json" };
+import HedgeRiskMock from "./HedgeRiskMock.json" with { type: "json" };
+import HedgeTokenMock from "./HedgeTokenMock.json" with { type: "json" };
+import HedgeVaultMock from "./HedgeVaultMock.json" with { type: "json" };
+import IAaveV3Adapter from "./IAaveV3Adapter.json" with { type: "json" };
+import IAaveV3Pool from "./IAaveV3Pool.json" with { type: "json" };
+import IAccessControl from "./IAccessControl.json" with { type: "json" };
+import IChainlinkAggregatorV3 from "./IChainlinkAggregatorV3.json" with { type: "json" };
+import IERC1155Errors from "./IERC1155Errors.json" with { type: "json" };
+import IERC1363 from "./IERC1363.json" with { type: "json" };
+import IERC165 from "./IERC165.json" with { type: "json" };
+import IERC20 from "./IERC20.json" with { type: "json" };
+import IERC20Errors from "./IERC20Errors.json" with { type: "json" };
+import IERC20Metadata from "./IERC20Metadata.json" with { type: "json" };
+import IERC5267 from "./IERC5267.json" with { type: "json" };
+import IERC721Errors from "./IERC721Errors.json" with { type: "json" };
+import IFlashCallback from "./IFlashCallback.json" with { type: "json" };
+import IFlashLiquidityProvider from "./IFlashLiquidityProvider.json" with { type: "json" };
+import IIntentManager from "./IIntentManager.json" with { type: "json" };
+import ILayerZeroAdapter from "./ILayerZeroAdapter.json" with { type: "json" };
+import ILayerZeroEndpointV2 from "./ILayerZeroEndpointV2.json" with { type: "json" };
+import IMulticall3 from "./IMulticall3.json" with { type: "json" };
+import IOracleAdapter from "./IOracleAdapter.json" with { type: "json" };
+import IPositionManager from "./IPositionManager.json" with { type: "json" };
+import IPriceObserver from "./IPriceObserver.json" with { type: "json" };
+import IRecoveryExecutor from "./IRecoveryExecutor.json" with { type: "json" };
+import IRemoteExecutor from "./IRemoteExecutor.json" with { type: "json" };
+import IRiskController from "./IRiskController.json" with { type: "json" };
+import IStrategyAdapter from "./IStrategyAdapter.json" with { type: "json" };
+import IUniswapV4Adapter from "./IUniswapV4Adapter.json" with { type: "json" };
+import IUniswapV4Hook from "./IUniswapV4Hook.json" with { type: "json" };
+import IUniswapV4PoolManager from "./IUniswapV4PoolManager.json" with { type: "json" };
+import IVault from "./IVault.json" with { type: "json" };
+import IZKCollateralProofVerifier from "./IZKCollateralProofVerifier.json" with { type: "json" };
+import IZKIntentProofVerifier from "./IZKIntentProofVerifier.json" with { type: "json" };
+import IZKOwnershipProofVerifier from "./IZKOwnershipProofVerifier.json" with { type: "json" };
+import InsuranceReserve from "./InsuranceReserve.json" with { type: "json" };
+import IntentManager from "./IntentManager.json" with { type: "json" };
+import IntentManagerTest from "./IntentManagerTest.json" with { type: "json" };
+import IntentSettlement from "./IntentSettlement.json" with { type: "json" };
+import LayerZeroAdapter from "./LayerZeroAdapter.json" with { type: "json" };
+import LayerZeroAdapterTest from "./LayerZeroAdapterTest.json" with { type: "json" };
+import LendingManager from "./LendingManager.json" with { type: "json" };
+import LendingManagerTest from "./LendingManagerTest.json" with { type: "json" };
+import LendingToken from "./LendingToken.json" with { type: "json" };
+import LiquidityManager from "./LiquidityManager.json" with { type: "json" };
+import LiquidityManagerTest from "./LiquidityManagerTest.json" with { type: "json" };
+import LiquidityPoolManagerMock from "./LiquidityPoolManagerMock.json" with { type: "json" };
+import LiquidityToken from "./LiquidityToken.json" with { type: "json" };
+import MessageHashUtils from "./MessageHashUtils.json" with { type: "json" };
+import MockFlashProvider from "./MockFlashProvider.json" with { type: "json" };
+import MockGeneratedVerifier from "./MockGeneratedVerifier.json" with { type: "json" };
+import MockInsuranceToken from "./MockInsuranceToken.json" with { type: "json" };
+import MockLayerZeroEndpoint from "./MockLayerZeroEndpoint.json" with { type: "json" };
+import MockRecoveryExecutor from "./MockRecoveryExecutor.json" with { type: "json" };
+import MockRemoteTarget from "./MockRemoteTarget.json" with { type: "json" };
+import MockToken from "./MockToken.json" with { type: "json" };
+import Pausable from "./Pausable.json" with { type: "json" };
+import PoolManagerMock from "./PoolManagerMock.json" with { type: "json" };
+import PositionAutomation from "./PositionAutomation.json" with { type: "json" };
+import PositionManager from "./PositionManager.json" with { type: "json" };
+import PositionManagerTest from "./PositionManagerTest.json" with { type: "json" };
+import PriceObserverMock from "./PriceObserverMock.json" with { type: "json" };
+import RebalanceAutomation from "./RebalanceAutomation.json" with { type: "json" };
+import RecoveryAdapterMock from "./RecoveryAdapterMock.json" with { type: "json" };
+import RecoveryExecutor from "./RecoveryExecutor.json" with { type: "json" };
+import RecoveryExecutorTest from "./RecoveryExecutorTest.json" with { type: "json" };
+import RecoveryToken from "./RecoveryToken.json" with { type: "json" };
+import ReentrancyGuard from "./ReentrancyGuard.json" with { type: "json" };
+import RemoteExecutor from "./RemoteExecutor.json" with { type: "json" };
+import RiskController from "./RiskController.json" with { type: "json" };
+import RiskControllerTest from "./RiskControllerTest.json" with { type: "json" };
+import RiskLimitHook from "./RiskLimitHook.json" with { type: "json" };
+import RiskLimitHookTest from "./RiskLimitHookTest.json" with { type: "json" };
+import RiskMath from "./RiskMath.json" with { type: "json" };
+import RiskMathTest from "./RiskMathTest.json" with { type: "json" };
+import SafeCast from "./SafeCast.json" with { type: "json" };
+import SafeERC20 from "./SafeERC20.json" with { type: "json" };
+import ShortStrings from "./ShortStrings.json" with { type: "json" };
+import SolverRegistry from "./SolverRegistry.json" with { type: "json" };
+import SolverSettlement from "./SolverSettlement.json" with { type: "json" };
+import SolverSettlementTest from "./SolverSettlementTest.json" with { type: "json" };
+import StdAssertions from "./StdAssertions.json" with { type: "json" };
+import StdInvariant from "./StdInvariant.json" with { type: "json" };
+import StrategyAdapterMock from "./StrategyAdapterMock.json" with { type: "json" };
+import StrategyExecutor from "./StrategyExecutor.json" with { type: "json" };
+import StrategyExecutorTest from "./StrategyExecutorTest.json" with { type: "json" };
+import StrategyToken from "./StrategyToken.json" with { type: "json" };
+import Strings from "./Strings.json" with { type: "json" };
+import Test from "./Test.json" with { type: "json" };
+import UniswapToken from "./UniswapToken.json" with { type: "json" };
+import UniswapV4Adapter from "./UniswapV4Adapter.json" with { type: "json" };
+import UniswapV4AdapterTest from "./UniswapV4AdapterTest.json" with { type: "json" };
+import UnwindToken from "./UnwindToken.json" with { type: "json" };
+import Vault from "./Vault.json" with { type: "json" };
+import VaultTest from "./VaultTest.json" with { type: "json" };
+import Vm from "./Vm.json" with { type: "json" };
+import VmContractHelper104 from "./VmContractHelper104.json" with { type: "json" };
+import VmContractHelper32 from "./VmContractHelper32.json" with { type: "json" };
+import VmContractHelper43 from "./VmContractHelper43.json" with { type: "json" };
+import VmContractHelper44 from "./VmContractHelper44.json" with { type: "json" };
+import VmContractHelper45 from "./VmContractHelper45.json" with { type: "json" };
+import VmContractHelper46 from "./VmContractHelper46.json" with { type: "json" };
+import VmContractHelper47 from "./VmContractHelper47.json" with { type: "json" };
+import VmContractHelper55 from "./VmContractHelper55.json" with { type: "json" };
+import VmContractHelper61 from "./VmContractHelper61.json" with { type: "json" };
+import VmContractHelper62 from "./VmContractHelper62.json" with { type: "json" };
+import VmContractHelper63 from "./VmContractHelper63.json" with { type: "json" };
+import VmContractHelper64 from "./VmContractHelper64.json" with { type: "json" };
+import VmContractHelper65 from "./VmContractHelper65.json" with { type: "json" };
+import VmContractHelper66 from "./VmContractHelper66.json" with { type: "json" };
+import VmContractHelper69 from "./VmContractHelper69.json" with { type: "json" };
+import VmContractHelper72 from "./VmContractHelper72.json" with { type: "json" };
+import VmContractHelper79 from "./VmContractHelper79.json" with { type: "json" };
+import VmContractHelper83 from "./VmContractHelper83.json" with { type: "json" };
+import VmContractHelper86 from "./VmContractHelper86.json" with { type: "json" };
+import VmContractHelper90 from "./VmContractHelper90.json" with { type: "json" };
+import VmContractHelper94 from "./VmContractHelper94.json" with { type: "json" };
+import VmSafe from "./VmSafe.json" with { type: "json" };
+import ZKCollateralVerifier from "./ZKCollateralVerifier.json" with { type: "json" };
+import ZKIntentVerifier from "./ZKIntentVerifier.json" with { type: "json" };
+import ZKOwnershipVerifier from "./ZKOwnershipVerifier.json" with { type: "json" };
+import ZKVerifierTest from "./ZKVerifierTest.json" with { type: "json" };
+import stdError from "./stdError.json" with { type: "json" };
+import stdStorageSafe from "./stdStorageSafe.json" with { type: "json" };
+
+export type AbiItem = Record<string, unknown>;
+export type ContractAbi = readonly AbiItem[];
+
+export const abis = {
+  "AavePoolMock": AavePoolMock,
+  "AaveV3Adapter": AaveV3Adapter,
+  "AccessControl": AccessControl,
+  "AggregatorMock": AggregatorMock,
+  "ChainlinkOracle": ChainlinkOracle,
+  "ChainlinkOracleTest": ChainlinkOracleTest,
+  "CircuitBreaker": CircuitBreaker,
+  "CrossChainRouter": CrossChainRouter,
+  "DeltaMath": DeltaMath,
+  "DeltaMathHarness": DeltaMathHarness,
+  "DeltaMathTest": DeltaMathTest,
+  "ECDSA": ECDSA,
+  "EIP712": EIP712,
+  "ERC165": ERC165,
+  "ERC20": ERC20,
+  "EmergencyAutomation": EmergencyAutomation,
+  "EmergencyControlsTest": EmergencyControlsTest,
+  "EmergencyUnwind": EmergencyUnwind,
+  "EmergencyUnwindTest": EmergencyUnwindTest,
+  "FeeToken": FeeToken,
+  "HealthFactorMath": HealthFactorMath,
+  "HealthFactorMathHarness": HealthFactorMathHarness,
+  "HealthFactorMathTest": HealthFactorMathTest,
+  "HedgeAdapterMock": HedgeAdapterMock,
+  "HedgeManager": HedgeManager,
+  "HedgeManagerTest": HedgeManagerTest,
+  "HedgePositionMock": HedgePositionMock,
+  "HedgeRiskMock": HedgeRiskMock,
+  "HedgeTokenMock": HedgeTokenMock,
+  "HedgeVaultMock": HedgeVaultMock,
+  "IAaveV3Adapter": IAaveV3Adapter,
+  "IAaveV3Pool": IAaveV3Pool,
+  "IAccessControl": IAccessControl,
+  "IChainlinkAggregatorV3": IChainlinkAggregatorV3,
+  "IERC1155Errors": IERC1155Errors,
+  "IERC1363": IERC1363,
+  "IERC165": IERC165,
+  "IERC20": IERC20,
+  "IERC20Errors": IERC20Errors,
+  "IERC20Metadata": IERC20Metadata,
+  "IERC5267": IERC5267,
+  "IERC721Errors": IERC721Errors,
+  "IFlashCallback": IFlashCallback,
+  "IFlashLiquidityProvider": IFlashLiquidityProvider,
+  "IIntentManager": IIntentManager,
+  "ILayerZeroAdapter": ILayerZeroAdapter,
+  "ILayerZeroEndpointV2": ILayerZeroEndpointV2,
+  "IMulticall3": IMulticall3,
+  "IOracleAdapter": IOracleAdapter,
+  "IPositionManager": IPositionManager,
+  "IPriceObserver": IPriceObserver,
+  "IRecoveryExecutor": IRecoveryExecutor,
+  "IRemoteExecutor": IRemoteExecutor,
+  "IRiskController": IRiskController,
+  "IStrategyAdapter": IStrategyAdapter,
+  "IUniswapV4Adapter": IUniswapV4Adapter,
+  "IUniswapV4Hook": IUniswapV4Hook,
+  "IUniswapV4PoolManager": IUniswapV4PoolManager,
+  "IVault": IVault,
+  "IZKCollateralProofVerifier": IZKCollateralProofVerifier,
+  "IZKIntentProofVerifier": IZKIntentProofVerifier,
+  "IZKOwnershipProofVerifier": IZKOwnershipProofVerifier,
+  "InsuranceReserve": InsuranceReserve,
+  "IntentManager": IntentManager,
+  "IntentManagerTest": IntentManagerTest,
+  "IntentSettlement": IntentSettlement,
+  "LayerZeroAdapter": LayerZeroAdapter,
+  "LayerZeroAdapterTest": LayerZeroAdapterTest,
+  "LendingManager": LendingManager,
+  "LendingManagerTest": LendingManagerTest,
+  "LendingToken": LendingToken,
+  "LiquidityManager": LiquidityManager,
+  "LiquidityManagerTest": LiquidityManagerTest,
+  "LiquidityPoolManagerMock": LiquidityPoolManagerMock,
+  "LiquidityToken": LiquidityToken,
+  "MessageHashUtils": MessageHashUtils,
+  "MockFlashProvider": MockFlashProvider,
+  "MockGeneratedVerifier": MockGeneratedVerifier,
+  "MockInsuranceToken": MockInsuranceToken,
+  "MockLayerZeroEndpoint": MockLayerZeroEndpoint,
+  "MockRecoveryExecutor": MockRecoveryExecutor,
+  "MockRemoteTarget": MockRemoteTarget,
+  "MockToken": MockToken,
+  "Pausable": Pausable,
+  "PoolManagerMock": PoolManagerMock,
+  "PositionAutomation": PositionAutomation,
+  "PositionManager": PositionManager,
+  "PositionManagerTest": PositionManagerTest,
+  "PriceObserverMock": PriceObserverMock,
+  "RebalanceAutomation": RebalanceAutomation,
+  "RecoveryAdapterMock": RecoveryAdapterMock,
+  "RecoveryExecutor": RecoveryExecutor,
+  "RecoveryExecutorTest": RecoveryExecutorTest,
+  "RecoveryToken": RecoveryToken,
+  "ReentrancyGuard": ReentrancyGuard,
+  "RemoteExecutor": RemoteExecutor,
+  "RiskController": RiskController,
+  "RiskControllerTest": RiskControllerTest,
+  "RiskLimitHook": RiskLimitHook,
+  "RiskLimitHookTest": RiskLimitHookTest,
+  "RiskMath": RiskMath,
+  "RiskMathTest": RiskMathTest,
+  "SafeCast": SafeCast,
+  "SafeERC20": SafeERC20,
+  "ShortStrings": ShortStrings,
+  "SolverRegistry": SolverRegistry,
+  "SolverSettlement": SolverSettlement,
+  "SolverSettlementTest": SolverSettlementTest,
+  "StdAssertions": StdAssertions,
+  "StdInvariant": StdInvariant,
+  "StrategyAdapterMock": StrategyAdapterMock,
+  "StrategyExecutor": StrategyExecutor,
+  "StrategyExecutorTest": StrategyExecutorTest,
+  "StrategyToken": StrategyToken,
+  "Strings": Strings,
+  "Test": Test,
+  "UniswapToken": UniswapToken,
+  "UniswapV4Adapter": UniswapV4Adapter,
+  "UniswapV4AdapterTest": UniswapV4AdapterTest,
+  "UnwindToken": UnwindToken,
+  "Vault": Vault,
+  "VaultTest": VaultTest,
+  "Vm": Vm,
+  "VmContractHelper104": VmContractHelper104,
+  "VmContractHelper32": VmContractHelper32,
+  "VmContractHelper43": VmContractHelper43,
+  "VmContractHelper44": VmContractHelper44,
+  "VmContractHelper45": VmContractHelper45,
+  "VmContractHelper46": VmContractHelper46,
+  "VmContractHelper47": VmContractHelper47,
+  "VmContractHelper55": VmContractHelper55,
+  "VmContractHelper61": VmContractHelper61,
+  "VmContractHelper62": VmContractHelper62,
+  "VmContractHelper63": VmContractHelper63,
+  "VmContractHelper64": VmContractHelper64,
+  "VmContractHelper65": VmContractHelper65,
+  "VmContractHelper66": VmContractHelper66,
+  "VmContractHelper69": VmContractHelper69,
+  "VmContractHelper72": VmContractHelper72,
+  "VmContractHelper79": VmContractHelper79,
+  "VmContractHelper83": VmContractHelper83,
+  "VmContractHelper86": VmContractHelper86,
+  "VmContractHelper90": VmContractHelper90,
+  "VmContractHelper94": VmContractHelper94,
+  "VmSafe": VmSafe,
+  "ZKCollateralVerifier": ZKCollateralVerifier,
+  "ZKIntentVerifier": ZKIntentVerifier,
+  "ZKOwnershipVerifier": ZKOwnershipVerifier,
+  "ZKVerifierTest": ZKVerifierTest,
+  "stdError": stdError,
+  "stdStorageSafe": stdStorageSafe,
+} as const satisfies Record<string, ContractAbi>;
+
+export function getAbi(name: string): ContractAbi {
+  const abi = abis[name as keyof typeof abis];
+  if (!abi) throw new Error(`ABI not generated for contract: ${name}`);
+  return abi;
+}
